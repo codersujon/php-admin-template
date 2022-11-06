2dc9a2341b6938d2add79fc32fb50adaa1e2b502
MD.SUJAN <csesujon155@gmail.com>
2022-11-07 03:45:53 +0600

    ready admin template mastering

---
+++ manageproduct.php
@@ -0,0 +1,56 @@
+<?php 
+    include "includes/header.php";
+    include "includes/preloader.php";
+?>
+
+<!-- Navbar -->
+<?php include "includes/navbar.php";?>
+<!-- /.navbar -->
+
+<!-- Sidebar -->
+<?php include "includes/sidebar.php";?>
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Manage Product</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Manage Product</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+
+      <!-- Main row -->
+      <div class="row">
+        <div class="col-12">
+              <h1 class="display-1 text-light text-center">Manage Product</h1>
+        </div>
+      </div>
+      <!-- /.row -->
+
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+
+<!-- Main Footer -->
+<?php include "includes/footer.php";?>
+
+<!-- Script Files -->
+<?php include "includes/scripts.php";?>
